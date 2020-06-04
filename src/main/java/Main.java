4c81fa858149bbab0a7895a2d85ad2678d167c37
yanakarmanova <karmanova.yana@gmail.com>
2020-06-04 22:33:29 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Account db = new Account(123);
+        Account n26 = new Account(987);
+        Customer yana = new Customer("Yana", "Karmanova", db);
+        Customer stefan = new Customer("Stefan", "Kraus", n26);
+
+        yana.getAccount().deposit(90);
+        stefan.getAccount().deposit(70);
+
+        yana.getAccount().withdraw(23);
+        stefan.getAccount().withdraw(100);
+
+        yana.getAccount().transfer(n26, 30);
+
+        BankService bank = new BankService();
+
+        bank.addPayment(new Payment(1, 5, Category.INCOME, 4, 2019));
+        bank.addPayment(new Payment(2, -10.5, Category.CAFE, 4, 2019));
+        bank.addPayment(new Payment(3, -4.5, Category.TRANSPORT, 4, 2019));
+        bank.addPayment(new Payment(4, 33, Category.INCOME, 5, 2019));
+        bank.addPayment(new Payment(5, 7, Category.INCOME, 5, 2019));
+        bank.addPayment(new Payment(6, -5, Category.CAFE, 5, 2019));
+        bank.addPayment(new Payment(7, 20, Category.INCOME, 4, 2020));
+        bank.addPayment(new Payment(8, 3, Category.INCOME, 4, 2020));
+        bank.addPayment(new Payment(9, -15, Category.TRANSPORT, 4, 2020));
+
+        System.out.println(bank.biggestPayment());
+        System.out.println(bank.totalIncome());
+        bank.alphGrouping();
+        bank.selectStatistics();
+        bank.biggestIncome();
+        bank.totalSpending();
+        bank.totalSpendingByMonth();
+    }
+}

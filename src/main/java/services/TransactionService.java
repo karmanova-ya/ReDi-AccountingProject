5d64be5dabdf83985c917921da36120ffeb656a5
yanakarmanova <karmanova.yana@gmail.com>
2020-06-09 00:04:35 +0200

    addPayment method is works correctly

--- src/main/java/services/TransactionService.java
+++ src/main/java/services/TransactionService.java
@@ -14,49 +14,59 @@ public class TransactionService {
         System.out.println(money + "€ has been transferred to your " + accTo.getBankName() + " account");
     }
 
-    public void addPayment() { // Unit testing for separate parts in separate methods
-        //double amount, Category cat, int month, int year
+    public PaymentCategory addPaymentCategory() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Select your spending category: ");
+        System.out.println("ENTERTAINMENT (e), CAFE(c), TRANSPORT(t), GROCERY(g)");
+        String cat = input.next();
+        PaymentCategory payCat = null;
 
         boolean isContinue = true;
         while (isContinue) {
-            System.out.println("Select your spending category: ");
-            System.out.println("ENTERTAINMENT (e), CAFE(c), TRANSPORT(t), GROCERY(g)");
-            String cat = input.next();
-            PaymentCategory payCat = null;
-            Payment payment = new Payment();
-
             switch (cat) {
                 case "e":
                     payCat = PaymentCategory.ENTERTAINMENT;
-                    payment.setPaymentCategory(payCat);
+                    isContinue = false;
                     break;
                 case "c":
                     payCat = PaymentCategory.CAFE;
-                    payment.setPaymentCategory(payCat);
+                    isContinue = false;
                     break;
                 case "t":
                     payCat = PaymentCategory.TRANSPORT;
-                    payment.setPaymentCategory(payCat);
+                    isContinue = false;
                     break;
                 case "g":
                     payCat = PaymentCategory.GROCERY;
-                    payment.setPaymentCategory(payCat);
+                    isContinue = false;
                     break;
                 default:
-                    System.out.println("Please clarify your answer\n");
+                    System.out.println("Invalid category. Please check your input\n");
+                    System.out.println("Select your spending category: ");
+                    System.out.println("ENTERTAINMENT (e), CAFE(c), TRANSPORT(t), GROCERY(g)");
+                    cat = input.next();
             }
         }
-        double amount = input.nextDouble();
-        String cat = input.next();
-        PaymentCategory paymentCategory = PaymentCategory.valueOf(cat.toUpperCase());
-        if(paymentCategory == null) {
-            System.out.println("Invalid category. Please check your input");
-        }
-        int month = input.nextInt();
-        int year = input.nextInt();
-
+        return payCat;
     }
+
+    //double amount, Category cat, int month, int year
+    public void addPayment() {   // Unit testing for separate parts in separate methods
+        Scanner input = new Scanner(System.in);
+        Payment payment = new Payment();
+        System.out.print("Enter amount -> ");
+        double amount = input.nextDouble();
+        payment.setPaymentCategory(addPaymentCategory());
+        System.out.print("Enter month -> ");
+        int month = input.nextInt();
+        System.out.print("Enter year -> ");
+        int year = input.nextInt();
+        payment.setAmount(-amount);
+        payment.setMonth(month);
+        payment.setYear(year);
+        System.out.println("You add a payment: " + payment.getPaymentCategory() + " = " + payment.getAmount() + "€ - " + payment.getMonth() + "." + payment.getYear());
+    }
+
 
     public void addIncome() {   // testing
     }
